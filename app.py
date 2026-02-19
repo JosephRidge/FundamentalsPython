cc6adc0b1b91e0ede5f7547e844880d18fba7add
JosephRidge <josephridge18@gmail.com>
2026-02-19 16:06:15 +0300

    arithmetic operators

---
+++ app.py
@@ -0,0 +1,63 @@
+# this is a commment
+"""
+Python is a high-level programming language created by Guido van Rossum and first
+released in 1991. Its reputation is growing, as today, various training institutes are
+covering the language as part of their data science prospectus.
+"""
+
+"""
+This is a multi-line string/ comment
+
+"""
+
+# VARIABLES: host references to something stored in memory
+age = 12 # variable
+name = "John Doe"
+name = "Michael Angelo" # variable overriding
+weight = 20.5
+output =""  
+
+ 
+"""
+DATA TYPES:
+    - NUMBERS: integers, float, complex
+    - TEXT: strings str()
+    - LIST:list, set, tuple, dictioary
+"""
+
+# integer: int()
+numberOne = 20 # integer
+output = type(numberOne) # checking the data type
+ 
+numberTwo = 100000000000000
+output = type(numberTwo)
+
+output = numberOne
+output = numberTwo
+
+"""
+ARITHMETIC OPERATIONS:
+    - addition(+)
+    - subtraction(-)
+    - multiplication(*)
+    - division(/)
+    - power(**)
+    - floor division(//)
+    - modulus(%)
+"""
+x = 10 
+y = 12
+
+output = x + y # addition
+
+
+
+
+
+
+
+
+print("==================")
+print(output) # standard output
+print("==================")
+
