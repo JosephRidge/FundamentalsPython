cf40bbe5eade5b149c228d1a15f29ce072c5aa8d
JosephRidge <josephridge18@gmail.com>
2026-02-24 15:43:51 +0300

    update

--- lists.py
+++ lists.py
@@ -6,7 +6,7 @@ LISTS:
     - dictionary
 """
 
-fruits = ["apple", "mango", "bananas", 2,3, 4.56] #list
+fruits = ["apple", "mango", "bananas"] #list
 output = fruits[1]
 output = fruits[2]
 
@@ -23,6 +23,24 @@ for student in students:
 
 for index, student in enumerate(students):
     print(f"{index + 1}. {student}")
+
+output = type(students)
+
+students = ["John", "Doe","Peter", "Martha", "Kome"]
+students[0] = "Parker"
+name = "Pete"
+students[3] = "Maria"
+output = students
+# name[0] = "R"
+
+colors = ["green","green", "green",  "blue", "blue","gray","purple","yellow"]
+output = colors.count("green") # check how many times has "green" been repeated
+
+
+# set: ==>
+
+
+
 
 
 print("===========================")
