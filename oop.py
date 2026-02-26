40e6d88d0c0811f90a672d549d5d578604753315
JosephRidge <josephridge18@gmail.com>
2026-02-26 14:47:09 +0300

    update

---
+++ oop.py
@@ -0,0 +1,14 @@
+"""
+OBJECT ORIENTED PROGRAMMING
+""" 
+
+"""
+Task 2: Loop + Logic (10 min)
+Print all numbers between 1 and 30 ( for number in ranger(1,30): 
+Print only numbers divisible by 3 (hint: use modulus) if( number % 3 == 0)
+Count how many numbers are divisible by 3
+
+"""
+
+for number in range(2, 30):
+    print(f"{number}/3 the remainer is {number%3}") 
